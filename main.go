5c119e01f0f9d6c42d389b704e40d7903a9386cf
Takuma TAKAKUSAGI <tak@moove.co.jp>
2026-05-01 16:33:23 +0900

    2問追加

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Question struct {
+	ID int `json:"id"`
+	Text string `json:"text"`
+	Choices []string `json:"choices"`
+	Answer string `json:"answer"`
+	Explain string `json:"explain"`
+}
+
+var questions = []Question {
+	{
+		ID: 1,
+		Text: "現在のディレクトリを表示するコマンドは？",
+		Choices: []string{"ls", "pwd", "cd", "find"},
+		Answer: "pwd",
+		Explain: "pwd は print working directory の略です。",
+	},
+	{
+		ID: 2,
+		Text: "ファイル一覧を表示するコマンドは？",
+		Choices: []string{"ls", "hoge", "mkdir", "find"},
+		Answer: "ls",
+		Explain: "ls はディレクトリ内のファイル一覧を表示します。",
+	},
+}
 
 func main() {
-	fmt.Println("Hello, Go")
+	http.HandleFunc("/questions", getQuestions)
+
+	log.Println("server start: http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func getQuestions(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	json.NewEncoder(w).Encode(questions)
 }
